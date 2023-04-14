4ac4e57c260dfd5c0233dcddd7b032fe7110a3a8
Mischa Spiegelmock <me@mish.dev>
2023-04-14 12:47:19 -0700

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# sdl3-rs
-SDL3 bindings for rust
+# SDL3 for Rust
+
+SDL3 bindings for rust.
+
+# Contributing
+
+Looking for contributors to help port the features in [sdl2-rs](https://github.com/Rust-SDL2/rust-sdl2/tree/master/src/sdl2)
+to Lokathor's new [sdl3-sys](https://github.com/Lokathor/sdl3-sys-rs) FFI bindings as they are developed and become stable.
+
+## Based on
+
+[sdl2-rs](https://github.com/Rust-SDL2/rust-sdl2)
+[Lokathor/sdl3-sys-rs](https://github.com/Lokathor/sdl3-sys-rs)
---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+#![crate_name = "sdl3"]
+#![crate_type = "lib"]
+
+pub extern crate libc;
+
+// #[macro_use]
+// extern crate lazy_static;
+
+pub extern crate sdl3_sys as sys;
+
+pub use crate::sdl::*;
+
+#[macro_use]
+// mod macros;
+// pub mod audio;
+// pub mod controller;
+// pub mod event;
+// pub mod filesystem;
+// pub mod haptic;
+// pub mod hint;
+// pub mod joystick;
+// pub mod keyboard;
+// pub mod log;
+// pub mod messagebox;
+// pub mod mouse;
+// pub mod pixels;
+// pub mod rect;
+// pub mod render;
+// pub mod rwops;
+mod sdl;
+// #[cfg(feature = "hidapi")]
+// pub mod sensor;
+// pub mod surface;
+// pub mod timer;
+// pub mod touch;
+// pub mod url;
+// pub mod version;
+// pub mod video;
+
+// modules
+// #[cfg(feature = "gfx")]
+// pub mod gfx;
+// #[cfg(feature = "image")]
+// pub mod image;
+// #[cfg(feature = "mixer")]
+// pub mod mixer;
+// #[cfg(feature = "ttf")]
+// pub mod ttf;
+
+// mod common;
+// // Export return types and such from the common module.
+// pub use crate::common::IntegerOrSdlError;
